aee17c342a1ddcf0bc1e94f04fd36d532d3025e7
JirehAgbozo <jireh.agbozo@edu.sait.ca>
2026-08-15 20:49:16 -0600

    feat: configure Supabase client with session persistence

---
+++ lib/supabase.ts
@@ -0,0 +1,19 @@
+import "react-native-url-polyfill/auto";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
+import { Platform } from "react-native";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabasePublishableKey =
+  process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    ...(Platform.OS !== "web" ? { storage: AsyncStorage } : {}),
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+});
